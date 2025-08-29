7b8dc861f00dc45a3f4e89e89579bd4ce4e1c34d
Anant Tiwari <ananttiwari231@gmail.com>
2025-08-29 18:29:02 +0530

    updated

---
+++ anant.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class anant
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Anant is learning Java");
+    }
+    }
